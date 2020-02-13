439fe50b9bd12e1cf361019ec16dd2832ff325e9
DanielTuttos <tutorialesdanielromero@gmail.com>
2020-02-13 15:55:27 -0500

    Primer Commit

---
+++ src/components/Resumen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import { primerMayuscula } from '../helper';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838f;
+    color: #fff;
+    margin-top:1rem;
+`;
+
+
+const Resumen = ({ datos }) => {
+
+    //extraer datos
+    const { marca, anio, plan } = datos;
+
+    if (marca === '' || anio === '' || plan === '') {
+        return null;
+    }
+    return (
+        <ContenedorResumen>
+
+            <h2>Resumen de Cotizacion</h2>
+            <ul>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+                <li>Año del auto: {anio}</li>
+            </ul>
+        </ContenedorResumen>
+    );
+}
+
+Resumen.propTypes={
+    datos:PropTypes.object.isRequired
+}
+
+export default Resumen;
